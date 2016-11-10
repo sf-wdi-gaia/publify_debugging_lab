7a9ae3bbaf45e2ae30f4bd355dee3032946a513c
brikky <brandon.kerr160@topper.wku.edu>
2016-11-10 10:55:01 -0800

    closes #4, months correctly associate with their articles

--- publify_core/app/models/archives_sidebar.rb
+++ publify_core/app/models/archives_sidebar.rb
@@ -30,7 +30,7 @@ class ArchivesSidebar < Sidebar
     article_counts = Content.find_by_sql(["select count(*) as count, #{date_func} from contents where type='Article' and published = ? and published_at < ? group by year,month order by year desc,month desc limit ? ", true, Time.now, count.to_i])
 
     @archives = article_counts.map do |entry|
-      month = (entry.month.to_i % 12) + 1
+      month = entry.month.to_i
       year = entry.year.to_i
       {
         name: I18n.l(Date.new(year, month), format: '%B %Y'),
